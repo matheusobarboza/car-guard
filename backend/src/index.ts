9cb0c0d768917aa59bcdce217058957b2d525c51
Matheus Barboza <matheus.barboza102@gmail.com>
2022-07-27 19:43:02 -0300

    feat: data-source, index

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { AppDataSource } from './data-source';
+import { routes } from './routes';
+
+export const app = express();
+
+AppDataSource.initialize().then(() => {
+  app.use(express.json());
+
+  app.use(routes);
+
+  return app.listen(process.env.PORT, () => console.log('Server is running!'));
+})
